ede93e936de995e8b5b688cc2931ad2fce2c624d
muertet <fp557s@gmail.com>
2022-01-12 22:24:16 +0100

    + Make server able to serve the client

--- webserver.js
+++ webserver.js
@@ -1,6 +1,8 @@
 
 const http = require("http");
+const fs = require('fs').promises;
 const Url = require('url');
+const path = require('path');
 const UserController = require("./controllers/User");
 
 class WebServer  {
@@ -9,6 +11,17 @@ class WebServer  {
         this.HTTP_STATUS_NO_CONTENT = 204;
         this.HTTP_STATUS_BAD_REQUEST = 400;
         this.HTTP_STATUS_NOT_FOUND = 404;
+        this.contentTypes = {
+            "jpg" : "image/jpeg",
+            "jpeg" : "image/jpeg",
+            "gif" : "image/gif",
+            "png" : "image/png",
+            "webp" : "image/webp",
+            "css" : "text/css",
+            "html" : "text/html",
+            "htm" : "text/html",
+            "js" : "application/x-javascript",
+        };
 
         this.server = http.createServer((req, res) => {
             let data = '';
@@ -35,7 +48,13 @@ class WebServer  {
             return this.reply(res, "", 0, this.HTTP_STATUS_NO_CONTENT);
         }
 
-        switch (parsedUrl.pathname.substring(1)) {
+        let route = parsedUrl.pathname.substring(1);
+
+        if (route.length === 0) {
+            route = "index.html";
+        }
+
+        switch (route) {
             case "api/login":
                 if (typeof data.username == "undefined" || typeof data.password == "undefined") {
                     return this.reply(res, "missing params", 1, this.HTTP_STATUS_BAD_REQUEST);
@@ -44,9 +63,30 @@ class WebServer  {
                 controller.login(data.username, data.password);
                 break;
             default:
-                this.reply(res, "not found", 1, this.HTTP_STATUS_NOT_FOUND);
+                fs.readFile("./client/" + route)
+                    .then(contents => {
+                        res.setHeader("Content-Type", this.getFileContentType(route));
+                        res.writeHead(200);
+                        res.end(contents);
+                    })
+                    .catch(err => {
+                        res.writeHead(404);
+                        res.end("not found");
+                        return;
+                    });
                 break;
         }
+    }
+
+    getFileContentType (fileName) {
+        const fileExtension = path.parse(fileName).ext.substring(1);
+
+        if (typeof this.contentTypes[fileExtension] === "undefined") {
+            console.error("Error extension " + fileExtension + " is not covered by webserver");
+            return "text/html";
+        }
+        
+        return this.contentTypes[fileExtension];
     }
 
     reply (res, message, error = 0, statusCode = 200) {
